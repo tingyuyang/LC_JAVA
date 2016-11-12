43c6de03dcd3d44f307149153c2e24079bf47696
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-12 02:35:11 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;// *this line is the point! to print whole list
+
+public class Main {
+	public static void main(String[] args) {
+    	System.out.println("hello world");
+    	int[] intArray = { 7, 9, 5, 1, 3 };
+		  System.out.println(Arrays.toString(intArray)); //*point
+	}
+}
