5b5582e7f44c123ef68b03a937f905b3aea26775
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-15 00:39:14 -0800

    Update 258. Add Digits.java

--- Easy/258. Add Digits.java
+++ Easy/258. Add Digits.java
@@ -1,8 +1,14 @@
+//method copy from python, 
+//java makes it so much more complicated....ewww...
+//string array to int, int to array...could not really manipulate together. annoying!
 public class Solution {
     public int addDigits(int num) {
         String[] numStr=Integer.toString(num).split("");
         int len =numStr.length;
         int result=0;
+        if (len==1){//make exception for integer is only 1 digits.
+            result= Integer.parseInt(numStr[0]);
+        }
         while (len!=1){
         	result=0;
         	for (int i=0;i<len;i++){
