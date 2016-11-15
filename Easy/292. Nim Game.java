85ab1514e39e7941c7f04579f6dcb1be8a5ee6a0
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-14 22:43:10 -0800

    Create 292. Nim Game.java

---
+++ Easy/292. Nim Game.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+// my solution is partially guessing. if the stone number is 4, 8, 12.... i will not win for sure.
+
+class Main {
+  public boolean nimGame(int n){
+  	boolean result=false;
+  	if (n<4){
+  		result= true;
+  	}
+  	else if (n%4!=0){
+  		result= true;
+  	}
+  	return result;
+  }
+  
+  public static void main(String[] args) {
+    String s ="hello";
+    Main m=new Main();
+    System.out.println(m.nimGame(4));
+  }
+  
+}
