893d76597f838e6ec72a3e7d87c511a621d4e8b0
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-15 00:01:35 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -37,31 +37,10 @@ class Main {
 //hello world  
 }
 
-//the difference of s.length() and array.length
-String s ="hello";
-System.out.println(s.length()); //* notice "()" for String s
-String[] array=s.split("");
-System.out.println(array.length);//* no "()" for String[] array
 
-//different print out
-String s ="hello";
-System.out.println(s);
 
-String[] array=s.split("");
-System.out.println(Arrays.toString(array));
 
-//declare int array
-int[] intArray = new int[] {4,5,6,7,8};
 
-//sort method
-public static void main(String[] args) {
-    Main m=new Main();
-    int[] num= new int[] {2,2,1,1,3};
-    Arrays.sort(num);//***
-    System.out.println(Arrays.toString(num));//***
-  }
 
-// int to string
-int num=38;
-String numStr=Integer.toString(num);//**NEED TO BE STORED
-System.out.println(numStr.length());
+
+
