a9e0da5fdabf4df99d5545f230d1b257ed2759f1
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-15 00:34:43 -0800

    Update 258. Add Digits.java

--- Easy/258. Add Digits.java
+++ Easy/258. Add Digits.java
@@ -1,17 +1,17 @@
-class Main {
-  public static void main(String[] args) {
-    int num=38;
-    String[] numStr=Integer.toString(num).split("");
-    int len =numStr.length();
-    int result=0;
-    while (len!=1){
-    	for (int i=0;i<len;i++){
-    		result+=Integer.parseInt(numStr[i]);
-    	}
-    	System.out.parseInt(result);
-    	numStr=Integer.toString(result).split("");
-		len=numStr.length
+public class Solution {
+    public int addDigits(int num) {
+        String[] numStr=Integer.toString(num).split("");
+        int len =numStr.length;
+        int result=0;
+        while (len!=1){
+        	result=0;
+        	for (int i=0;i<len;i++){
+        		result+=Integer.parseInt(numStr[i]);
+        	}
+        	numStr=Integer.toString(result).split("");
+    		len=numStr.length;
+        }
+        System.out.println(result);
+        return result;
     }
-    
-  }
 }
