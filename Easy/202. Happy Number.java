7a5a99b312ed6bf242d8b4c3a546bb71f20ea421
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-16 00:00:44 -0800

    Update 202. Happy Number.java

--- Easy/202. Happy Number.java
+++ Easy/202. Happy Number.java
@@ -4,33 +4,6 @@ http://www.cnblogs.com/grandyang/p/4447233.html ***
 http://www.cnblogs.com/grandyang/p/5560036.html
 */
 //i used set here
-public class Solution {
-    public boolean isHappy(int n) {
-        if (n==1){
-            return true;
-        }
-        int result=n;
-        int reminder=0;
-        Set<Integer> set = new HashSet<Integer>();
-        while (result!=1){ 
-              n=result;
-              result=0;
-              while (n!=0){
-                      reminder=n%10;
-                      n=n/10;
-                      result+=reminder*reminder;
-            }
-            if (result==1){
-              return true;
-            }else if (set.contains(result)){
-              return false;
-            }
-            set.add(result);
-        }
-    	return false;
-    }
-}
-
 //clean version:(same thought)
 class Main {
 	public boolean isHappy(int n) {
@@ -58,3 +31,32 @@ class Main {
 		System.out.println(m.isHappy(19));
   }
 }
+
+//first solution
+public class Solution {
+    public boolean isHappy(int n) {
+        if (n==1){
+            return true;
+        }
+        int result=n;
+        int reminder=0;
+        Set<Integer> set = new HashSet<Integer>();
+        while (result!=1){ 
+              n=result;
+              result=0;
+              while (n!=0){
+                      reminder=n%10;
+                      n=n/10;
+                      result+=reminder*reminder;
+            }
+            if (result==1){
+              return true;
+            }else if (set.contains(result)){
+              return false;
+            }
+            set.add(result);
+        }
+    	return false;
+    }
+}
+
