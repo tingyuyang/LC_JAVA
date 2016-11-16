6ca142bc3116dbb4ae1b021cab7e3f5853d41934
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-16 00:47:20 -0800

    Update 263. Ugly Number.java

--- Easy/263. Ugly Number.java
+++ Easy/263. Ugly Number.java
@@ -3,11 +3,11 @@
 //e.g. 2*5=10, return true. 
 public class Solution {
     public boolean isUgly(int n) {
-    while (n>=2){
-        if (n%2==0) n=n/2;
-        else if (n%3==0) n=n/3;
-        else if (n%5==0) n=n/5;
-        else return false;
+        while (n>=2){
+            if (n%2==0) n=n/2;
+            else if (n%3==0) n=n/3;
+            else if (n%5==0) n=n/5;
+            else return false;
         }
         return n==1;   
     }
