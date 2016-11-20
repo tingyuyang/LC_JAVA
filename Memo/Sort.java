9d5880c4abf99ffc8d0dcd1d501cf06ec70aba36
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-19 16:01:49 -0800

    Create Sort.java

---
+++ Memo/Sort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Main {
+  public static void main(String[] args) {
+    int[] number = {-1, 0, 1, 2, -1, -4};
+    Arrays.sort(number);
+    System.out.println(Arrays.toString(number));
+  }
+}
