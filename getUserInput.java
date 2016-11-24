66b5ab282e55f0d1be096208da730eb1f238b4a9
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-23 22:29:57 -0800

    Update getUserInput.java

--- getUserInput.java
+++ getUserInput.java
@@ -12,22 +12,18 @@ class Main {
 
 //while loop with user input
 import java.util.*;
-
-class Main {
-  public static void main(String[] args) {
-    System.out.println("if you want to quit, input 'q'");
-    Scanner input=new Scanner(System.in);
-    String s ="";       //*** pay attention to the doube quote ""
+public class Main{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    String s = "";    //*** pay attention to the doube quote ""
     while (true){
-      System.out.println("Do you want to quit? :");
-      s=input.nextLine();
-      if (s.equals("q")){   //***"q" double quote. not single quote.
-        System.out.println("Ok, you want to quit");
+      System.out.println("Please enter your input:");
+      s = input.nextLine();
+      if (s.equals("q")){     //***"q" double quote. not single quote.
+        System.out.println("Bye, You would like to quit.");
         break;
-      } else{
-        System.out.println("Ok,let's continue.");
       }
+      System.out.println("Ok, lets continue");
     }
-    System.out.println("Goodbye");
   }
 }
