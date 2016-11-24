f87691affed24716eec96f6d62266b5c7704aa56
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-23 21:00:04 -0800

    Create getUserInput.java

---
+++ getUserInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("if you want to quit, input 'q'");
+    Scanner input=new Scanner(System.in);
+    System.out.println("input:");
+    String s ="";
+    System.out.println("Your input is: "+input.nextLine());
+  }
+}
