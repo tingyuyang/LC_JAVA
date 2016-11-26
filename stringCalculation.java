ee23299a911e7bd57067854e1c35101d2e48e190
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-26 03:02:44 -0800

    Update stringCalculation.java

--- stringCalculation.java
+++ stringCalculation.java
@@ -3,7 +3,7 @@ class Main {//remember to figure out get rid of spaces
   public static void main(String[] args) {
     String name="1.3+5.1/3–0.8";//"figure out -1.3 later!
     String r = "";
-    double foo = 0;
+    double foo = 0; //attention to double here!
     for(int i= 0; i< name.length(); i++){
       if(name.charAt(i)!='/' && name.charAt(i)!='*' && name.charAt(i)!='+' && name.charAt(i)!='–'){
         r += Character.toString(name.charAt(i));
