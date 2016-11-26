a4dd4aae84d2347002e908850c6945ce67ad0359
Tingyu Yang <tingyuyang@lclark.edu>
2016-11-26 02:57:50 -0800

    Create stringCalculation.java

---
+++ stringCalculation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {//remember to figure out get rid of spaces
+  public static void main(String[] args) {
+    String name="1.3+5.1/3–0.8";//"figure out -1.3 later!
+    String r = "";
+    double foo = 0;
+    for(int i= 0; i< name.length(); i++){
+      if(name.charAt(i)!='/' && name.charAt(i)!='*' && name.charAt(i)!='+' && name.charAt(i)!='–'){
+        r += Character.toString(name.charAt(i));
+      } 
+      if(name.charAt(i)=='/' || name.charAt(i)=='*' || name.charAt(i)=='+' || name.charAt(i)=='–' || i==name.length()-1){
+        foo = Double.parseDouble(r);
+        r="";
+      }
+      System.out.println(foo);     
+    }
+  }
+}
